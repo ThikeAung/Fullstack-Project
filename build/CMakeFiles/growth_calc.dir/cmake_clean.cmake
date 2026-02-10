b615c26142f857ea783d5613b1c31d3bd92a9b07
Thike Aung <thikeaung@my.jcu.edu.au>
2026-02-10 21:22:14 +0700

    update

---
+++ build/CMakeFiles/growth_calc.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/growth_calc.dir/link.d"
+  "CMakeFiles/growth_calc.dir/main.cpp.o"
+  "CMakeFiles/growth_calc.dir/main.cpp.o.d"
+  "growth_calc"
+  "growth_calc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/growth_calc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
